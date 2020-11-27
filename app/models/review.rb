80f256a9e82cc891f178cde1c5b1197df1d83790
luis <luishenriquesimoes@yahoo.com.br>
2020-11-27 15:12:47 -0300

    seed criado

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,12 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: (0..5) }
   validates :content, presence: true
-  validate :valid_rating
-
-  def valid_rating
-    return if rating.is_a?(Integer) && rating >= 0 && rating <= 5
-
-    errors.add(:rating, 'Ratings must be numbers from 0 to 5')
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning the db...'
+
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'creating restaurants...'
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese belgian japanese french italian].sample
+  )
+  4.times do
+    review = Review.create(
+      rating: rand(0..5),
+      content: Faker::Quote.famous_last_words,
+      restaurant_id: restaurant.id
+    )
+    puts "Restaurant #{restaurant.id} created with review #{review.id}"
+  end
+end
